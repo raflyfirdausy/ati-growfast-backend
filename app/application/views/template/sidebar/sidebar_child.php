2e1efaa7f82e7a025db9e9daca20f561341a016f
Rafli Firdausy Irawan <rafly.firdausy@gmail.com>
2023-04-01 13:27:11 +0700

    change: apikey

--- app/application/views/template/sidebar/sidebar_child.php
+++ app/application/views/template/sidebar/sidebar_child.php
@@ -18,31 +18,3 @@
         <?php endif ?>
     </li>
 <?php endforeach ?>
-
-<!-- <li class="sidebar-item">
-    <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
-        <i class="mdi mdi-code-equal"></i>
-        <span class="hide-menu">Wisata</span>
-    </a>
-    <ul aria-expanded="false" class="collapse first-level">
-        <li class="sidebar-item">
-            <a href="<?= base_url('master/wisata/kategori') ?>" class="sidebar-link">
-                <i class="mdi mdi-adjust"></i>
-                <span class="hide-menu"> Kategori Wisata </span>
-            </a>
-        </li>
-        <li class="sidebar-item">
-            <a href="<?= base_url('master/wisata/data') ?>" class="sidebar-link">
-                <i class="mdi mdi-adjust"></i>
-                <span class="hide-menu"> Data & Foto Wisata </span>
-            </a>
-        </li>
-    </ul>
-</li>
-
-<li class="sidebar-item">
-    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="<?= base_url('dashboard') ?>" aria-expanded="false">
-        <i class="mdi mdi-code-equal"></i>
-        <span class="hide-menu">Dashboard</span>
-    </a>
-</li> -->
